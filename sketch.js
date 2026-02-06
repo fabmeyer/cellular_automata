40e8904a0b8ff538bf7ef5c4242f47f6c445e378
Fabian Meyer <fabian@meyerfoto.ch>
2026-02-06 10:21:36 +0100

    added new_grid

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 class Cell {
-    constructor(x, y, size, state) {
+    constructor(x, y, size, state, age) {
         this.x = x;
         this.y = y;
         this.size = size;
         this.state = state;
+        this.age = age;
     }
 }
 
@@ -14,28 +15,37 @@ function get_color_from_state(state) {
     if (state === 1) {
         return color("black");
     }
+    if (state === 2) {
+        return color("red");
+    }
 }
 
-let cols = 10;
-let rows = 10;
+// grid parameters
+let cols = 100;
+let rows = 100;
+let cell_size = 5;
 
-let cell_size = 20;
 // create a 2D array with given number of columns and rows
 let grid = new Array(cols).fill().map(() => new Array(rows));
+
+// simulation parameters
+let time_interval = 100;
+let percent_alive = 0.2;
+let max_age = 7;
 
 function initializeGrid() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let x = i * cell_size;
             let y = j * cell_size;
-            // let state = 0; // default state
-            randomn = random(100);
-            if (randomn < 10) {
+            let state = 0; // default state
+            let randomn = random(0, 99);
+            if (randomn < percent_alive * 100) {
                 state = 1; // black
             } else {
                 state = 0; // white
             }
-            grid[i][j] = new Cell(x, y, cell_size, state);
+            grid[i][j] = new Cell(x, y, cell_size, state, 0);
         }
     }
 }
@@ -44,7 +54,7 @@ function drawGrid() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let cell = grid[i][j];
-            console.log(cell.state);
+            // console.log(cell.state);
             fill(get_color_from_state(cell.state));
             rect(cell.x, cell.y, cell.size, cell.size);
         }
@@ -55,53 +65,107 @@ function setup() {
     createCanvas(cols * cell_size, rows * cell_size);
     initializeGrid();
     drawGrid();
-    setTimeout(nextFrame, 1000);
+    setTimeout(nextFrame, time_interval);
     nextFrame();
 }
 
 function nextFrame() {
-    setTimeout(nextFrame, 1000); // call nextFrame every 1000 milliseconds
     // update state function
     updateState();
     drawGrid();
+    setTimeout(nextFrame, time_interval); // call nextFrame every 1000 milliseconds
+}
+
+function wrapIndex(idx, max) {
+    return (idx + max) % max;
 }
 
 function updateState() {
+    new_grid = new Array(cols).fill().map(() => new Array(rows));
         for (let i = 0; i < cols; i++) {
             for (let j = 0; j < rows; j++) {
-            let cell = grid[i][j];
-            // get all 8 neighbors
-            let neighbors = [];
-            for (let x = -1; x <= 1; x++) {
-                for (let y = -1; y <= 1; y++) {
-                    if (x === 0 && y === 0) continue; // skip the cell itself
-                    let neighbor_x, neighbor_y;
-                    // check if neighbor is not within bounds of the grid
-                    if (i + x < 0 || i + x >= cols) {
-                        neighbor_x = (i + x + cols) % cols; // wrap around horizontally
-                    } else {
-                        neighbor_x = i + x;
+                let cell = grid[i][j];
+                let new_cell = cell; // copy current cell
+                // get all 8 neighbors
+                let neighbors = [];
+                for (let x = -1; x <= 1; x++) {
+                    for (let y = -1; y <= 1; y++) {
+                        if (x === 0 && y === 0) continue; // skip the cell itself
+                        const neighbor_x = wrapIndex(i + x, cols);
+                        const neighbor_y = wrapIndex(j + y, rows);
+                        neighbors.push(grid[neighbor_x][neighbor_y]);
                     }
-                    if (j + y < 0 || j + y >= rows) {
-                        neighbor_y = (j + y + rows) % rows; // wrap around vertically
-                    } else {
-                        neighbor_y = j + y;
-                    }
-                    neighbors.push(grid[neighbor_x][neighbor_y]);
                 }
-            }
-
-            // for testing only
-            // randomn = random(100);
-            // if (randomn < 10) {
-            //     state = 1; // black
-            // } else {
-            //     state = 0; // white
-            // }
-            // cell.state = state;
+                
+                
+                let [next_state, next_age] = rule(cell, neighbors);
+                new_cell.state = next_state;
+                new_cell.age = next_age;
+                
+                new_grid[i][j] = new_cell;
+                // for testing only
+                // let randomn = random(100);
+                // if (randomn < percent_alive * 100) {
+                //     state = 1; // black
+                // } else {
+                //     state = 0; // white
+                // }
+                // cell.state = state;
         }
     }
-}   
+    grid = new_grid;
+}
+
+function rule(cell, neighbors) {
+    // implement your rule here
+    let n_alive = 0;
+    for (let k = 0; k < neighbors.length; k++) {
+        if (neighbors[k].state === 1) {
+            n_alive += 1;
+        }
+    }
+
+    let next_state = 0;
+    let next_age = cell.age;
+
+    // update age
+    if (cell.state === 1 || cell.state === 2) {
+        next_age += 1;
+    } else if (cell.state === 0) {
+        next_age = 0;
+    }
+
+    if (cell.state === 0) {
+        next_age = 0;
+        if (n_alive === 3) {
+            next_state = 1;
+        }
+    }
+
+    if (cell.state === 1) {
+        next_age += 1;
+        if (n_alive === 2 || n_alive === 3) {
+            next_state = 1;
+        } else {
+            next_state = 0;
+        }
+        if (next_age >= max_age) {
+            next_state = 2;
+            next_age = 0;
+        }
+    }
+
+    if (cell.state === 2) {
+        next_age += 1;
+        if (next_age >= max_age) {
+        next_state = 0;
+        } else {
+            next_state = 2;
+        }
+    }
+
+    return [next_state, next_age];
+}
 
 // function mouseMoved() {
 //     if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
